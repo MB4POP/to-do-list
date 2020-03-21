8d5668ec311728592f14fab491fd1249acd26e9a
Mariusz Białecki <mariusz.bialecki@onet.pl>
2020-03-21 19:40:29 +0100

    add init form

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
+const tasks = [];
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('new user has connected');
+  socket.emit('updateData', tasks)
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task, 'emitted');
+  });
+
+  socket.on('removeTask', (id) => {
+    tasks.forEach((item, index) => {
+      if( item.id === id) {
+        tasks.splice(index, 1);
+      };
+    });
+    socket.broadcast.emit('removeTask', id, 'emitted');
+  });
+});
